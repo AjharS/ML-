6e436de756521d4a4519b09979d739b8a4e585b2
ajhars <ajhargshaikh@gmail.com>
2024-01-09 19:17:07 +0530

    Update test_proc.sql

--- test_proc.sql
+++ test_proc.sql
@@ -1,5 +1,5 @@
 create procedure test_proc.cicd_test_proc() 
 as
 begin
-create or replace table test_tbl.cicd_test_tbl as select 1 as id;
+create or replace table test_tbl.cicd_test_tbl as select 1 as id,'Hi' as msg;
 end
